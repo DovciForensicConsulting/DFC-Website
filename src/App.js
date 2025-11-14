1b0189577a5d8917ac5a678d72e41648f1306f34
TheChildishMillennial <thechildishmillennial@gmail.com>
2025-11-13 17:41:51 -0800

    Fixed analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import { SpeedInsights } from "@vercel/speed-insights/react"
-import { Analytics } from "@vercel/analytics/react"
+
 function App() {
   return (
     <div
       className='App'
     >
-      <SpeedInsights/>
-      <Analytics/>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import { router } from './lib/Routes';
 import { RouterProvider } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { system } from './lib/Theme';
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider value={system}>
       <RouterProvider router={router}>
+        <SpeedInsights/>
+        <Analytics/>
         <App />
       </RouterProvider>
     </ChakraProvider>
